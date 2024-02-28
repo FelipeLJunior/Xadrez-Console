3436d385897d2c070b84d55fbdc3a406d4b9172a
Felipe L. Junior <felipeloiolaoliveirajr@gmail.com>
2024-02-28 09:16:37 -0300

    Criação da classe Posicao

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -1,7 +1,14 @@
-﻿internal class Program
+﻿using Tabuleiro;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        Posicao p;
+
+        p = new Posicao(3, 4);
+
+        Console.WriteLine(p);
+        Console.ReadKey();
     }
 }
